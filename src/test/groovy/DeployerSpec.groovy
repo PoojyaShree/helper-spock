b8b689e5929d61bf4eaa0ecc1ea0f3d1df680b27
PoojyaShree <87475087+PoojyaShree@users.noreply.github.com>
2022-02-02 14:18:01 +0530

    Update DeployerSpec.groovy

--- src/test/groovy/DeployerSpec.groovy
+++ src/test/groovy/DeployerSpec.groovy
@@ -1,1 +1,33 @@
+import com.homeaway.devtools.jenkins.testing.JenkinsPipelineSpecification
+
+public class helloSpec extends JenkinsPipelineSpecification {
+
+        def Jenkinsfile = null
+
+        /*public static class DummyException extends RuntimeException {
+                public DummyException(String _message) { super( _message ); }
+        }*/
+
+        def setup() {
+                Jenkinsfile = loadPipelineScriptForTest("/vars/hello.groovy")
+                //Jenkinsfile.getBinding().setVariable( "scm", null )
+                //explicitlyMockPipelineVariable("deploy")
+                /*getPipelineMock("load")("hello.groovy") /*>> {
+                        getPipelineMock("hello")
+                }*/
+        }
+        def "sampletest"() {
+         when:
+                Jenkinsfile.deploy("test")
+        then:
+                  1 * getPipelineMock("echo")("hello test")
+            }
+        def "sampleproduction"() {
+          when:
+                Jenkinsfile.deploy("production")
+          then:
+                 1 * getPipelineMock("echo")("hello prod")
+                       }
+
+}
 
